bca218b80a4b292eec7fd592145c66aec89ab6d6
Lars van Rhijn <larsvanrhijn@gmail.com>
2020-04-29 10:57:46 +0200

    Set CAPTCHA score to 0.7

--- djangfolio/djangfolio/settings/base.py
+++ djangfolio/djangfolio/settings/base.py
@@ -112,4 +112,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
-RECAPTCHA_REQUIRED_SCORE = 0.85
+RECAPTCHA_REQUIRED_SCORE = 0.7
