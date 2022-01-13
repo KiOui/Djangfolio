2fbe8fe867598e7702b3490ddbefe5cd7b053de3
Lars van Rhijn <larsvanrhijn@gmail.com>
2022-01-13 08:02:24 +0100

    Upgraded to Django 3.2

--- website/djangfolio/settings/base.py
+++ website/djangfolio/settings/base.py
@@ -105,6 +105,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
@@ -114,7 +116,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
-print(MEDIA_ROOT)
 
 # Sites app
 SITE_ID = 1
